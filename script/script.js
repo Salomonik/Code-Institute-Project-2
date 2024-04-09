7569363889734d9566de060fe7fdbbf775fadfa4
Salomonik <salomonik@gmail.com>
2024-04-09 16:47:26 +0100

    add: functions to start game in script.js

---
+++ script/script.js
@@ -0,0 +1,59 @@
+let buttons = document.querySelectorAll('.game-container');
+buttons.forEach(button => {
+	button.addEventListener('click', async () => {
+		const gameId = button.id; // upewnij się, że każdy przycisk ma ustawione id
+		await loadingScreen();
+		showInstruction(gameId);
+		gameSelection(gameId);
+	});
+});
+
+
+
+
+function gameSelection(gameId) {
+	const gameScreen = document.querySelector('.game-screen');
+	gameScreen.innerHTML = ''; // Czyszczenie zawartości
+
+	switch (gameId) {
+		case 'game1': // Użycie stringów jako identyfikatorów
+			game1();
+			break;
+		case 'game2':
+			game2();
+			break;
+		case 'game3':
+			game3();
+			break;
+		default:
+			break;
+	}
+}
+
+function showInstruction(gameId) {
+	const instructions = document.querySelector('.instruction-screen');
+	instructions.textContent = '';
+}
+
+function loadingScreen() {
+    return new Promise((resolve, reject) => {
+        const screen = document.querySelector('.game-screen');
+        screen.style.backgroundColor = 'black';
+        screen.innerHTML = `
+            <div id="myProgress">LOADING...
+                <div id="myBar"></div>
+            </div>`;
+
+        let width = 1;
+        let id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                resolve(); // Zakończ promise po załadowaniu paska
+            } else {
+                width++;
+                document.getElementById("myBar").style.width = width + "%";
+            }
+        }
+    });
+}
